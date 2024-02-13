dcb781f6816e5feda50d7f3432c9c7f09f2e07c7
ShivamSikotra11 <sikotra756@gmail.com>
2024-02-14 00:35:24 +0530

    Styled Components ADded

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,11 @@
+import styled from "styled-components";
+
 const Home = () => {
-  return <div>Home</div>;
+  return <Wrapper className="test">Home</Wrapper>;
 };
+
+const Wrapper = styled.section`
+  background-color: ${({ theme }) => theme.colors.bg};
+`;
 
 export default Home;
