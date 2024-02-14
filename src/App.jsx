7e48a18a73ba89d0c33c316b80fb57fd859a4285
ShivamSikotra11 <sikotra756@gmail.com>
2024-02-15 00:26:46 +0530

    Navbar Created

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Cart from "./Cart";
 import ErrorPage from "./ErrorPage";
 import { GlobalStyle } from "./GlobalStyle";
 import { ThemeProvider } from "styled-components";
+import Header from "./components/Header";
 
 const App = () => {
   const theme = {
@@ -39,6 +40,7 @@ const App = () => {
     <ThemeProvider theme={theme}>
       <Router>
         <GlobalStyle />
+        <Header></Header>
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/about" element={<About />}></Route>
